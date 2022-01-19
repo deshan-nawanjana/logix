b612fdc6180f451a01233c27a1cde427b097fafe
Deshan Nawanjana <deshan.uok@gmail.com>
2022-01-19 16:28:26 +0530

    Add Auto Scan Mode to Database

---
+++ database/auto.h
@@ -0,0 +1,9 @@
+#define SERIES_AUTO_COUNT 1
+
+int T_AUTO[] = { 0 };
+int I_AUTO[] = { 0 };
+int G_AUTO[] = { 0 };
+
+IC SERIES_AUTO[] = {
+	{ "????", T_AUTO, I_AUTO, G_AUTO }
+};
--- database/imports.h
+++ database/imports.h
@@ -1,12 +1,14 @@
-#define DATABASE_COUNT 2
+#define DATABASE_COUNT 3
 
 #include "4000.h"
 #include "7400.h"
+#include "auto.h"
 
 int db_index = 0;
 int ic_index = 0;
 
 DB DATABASE[DATABASE_COUNT] = {
     { "TTL", "7400", SERIES_7400, SERIES_7400_COUNT },
-    { "CMOS", "4000", SERIES_4000, SERIES_4000_COUNT }
+    { "CMOS", "4000", SERIES_4000, SERIES_4000_COUNT },
+    { "AUTO", "????", SERIES_AUTO, 1 }
 };
