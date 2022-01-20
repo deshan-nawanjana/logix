a47810454acd8640b660db18de92fc10b86c7523
Deshan Nawanjana <deshan.uok@gmail.com>
2022-01-21 00:26:57 +0530

    Add Connection Fetch Functions

--- client/logix.py
+++ client/logix.py
@@ -1,11 +1,62 @@
-import serial
+from tkinter import *
+import serial.tools.list_ports
 
-serialPort = serial.Serial(port = "COM4", baudrate = 9600, timeout = 2)
+vert = "LOGIX-1.0" # logix device version name
+port = None # serial object
+rate = 9600 # serial port baudrate
+name = None # arduino board name
 
-while 1:
-    val = serialPort.readline()
-    if(len(val) > 0) :
-        print(val)
-        
+root = Tk()
+root.geometry("650x400")
 
-x = input()
+def updateRoot() :
+    global root
+    try :
+        root.update()
+    except :
+        exit()
+
+def waitForConnection() :
+    global port
+    global rate
+    global name
+    global root
+    global vert
+    # wait for logix response from any com port
+    root.title('Waiting for connection...')
+    while port == None :
+        # check for available port that logix is connected
+        for srname in serial.tools.list_ports.comports() :
+            try :
+                # create port object
+                number = str(srname).split(' ')[0]
+                srport = serial.Serial(port = number, baudrate = rate)
+                data = srport.readline().decode().rstrip().lstrip()
+                if(data == vert) :
+                    port = srport
+                    name = srname
+                    root.title("LOGIX 1.0 Found")
+                    break
+                else :
+                    srport.close()
+            except :
+                continue
+        updateRoot()
+
+def waitForNextMessage() :
+    global port
+    try :
+        if port.isOpen() :
+            if port.in_waiting :
+                line = port.readline().decode().rstrip().lstrip()
+                root.title(line)
+        else :
+            waitForConnection()
+    except :
+        port = None
+        waitForConnection()
+    updateRoot()
+
+updateRoot()
+waitForConnection()
+while True : waitForNextMessage()
