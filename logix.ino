d4b75042b74e015336db0b6f08bff1091376c4d0
Deshan Nawanjana <deshan.uok@gmail.com>
2022-03-29 16:49:53 +0530

    Update Buttons Checker for PULLUP Mode

--- logix.ino
+++ logix.ino
@@ -16,6 +16,11 @@
 void setup() {
   displayInit();
   displayHome();
+  // button pin modes
+  pinMode(INPUT_PIN_LAST, INPUT_PULLUP);
+  pinMode(INPUT_PIN_NEXT, INPUT_PULLUP);
+  pinMode(INPUT_PIN_TYPE, INPUT_PULLUP);
+  pinMode(INPUT_PIN_SCAN, INPUT_PULLUP);
 }
 
 void loop() {
